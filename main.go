73d95bc092ba24c3417dfdb2094481e1bbb456ee
timolisa <timothyolisaeloka@gmail.com>
2023-07-15 11:56:08 +0100

    add a UserData struct

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
-	"strconv"
 )
 
 var conferenceName string = "Go conference"
@@ -12,7 +11,14 @@ const conferenceTickets int = 50
 
 var availableTickets uint = 50
 
-var bookings = make([]map[string]string, 0)
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName string
+	lastName string
+	email string
+	numberOfTickets int
+}
 
 func main() {
 
@@ -78,7 +84,7 @@ func getUserInput() (string, string, string, int) {
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		firstNames = append(firstNames, booking["firstName"])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
@@ -86,11 +92,12 @@ func getFirstNames() []string {
 func bookTickets(firstName string, lastName string, email string, userTickets int) {
 	availableTickets -= uint(userTickets)
 
-	var userData = make(map[string]string)
-	userData["firstName"] = firstName
-	userData["lastName"] = lastName
-	userData["email"] = email
-	userData["numberOfTickets"] = strconv.FormatInt(int64(userTickets), 10)
+	var userData = UserData {
+		firstName: firstName,
+		lastName: lastName,
+		email: email,
+		numberOfTickets: userTickets,
+	}
 
 	bookings = append(bookings, userData)
 
